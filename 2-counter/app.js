510719b2a3608e1d8aacbcdacf68ca79cc4a6e1f
Socrates Palma <dubstepero10@gmail.com>
2022-01-06 19:03:05 -0600

    finished first projects

---
+++ 2-counter/app.js
@@ -0,0 +1,24 @@
+const value = document.getElementById("value");
+const btns = document.querySelectorAll(".btn");
+
+let count = 0;
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const styles = e.currentTarget.classList;
+    // * setting the logic of the counter
+    styles.contains("decrease")
+      ? count--
+      : styles.contains("increase")
+      ? count++
+      : (count = 0);
+
+    // *  Changing colors according to value
+    count > 0
+      ? (value.style.color = "green")
+      : count < 0
+      ? (value.style.color = "red")
+      : (value.style.color = "#222");
+    value.textContent = count;
+  });
+});
---
+++ readme.md
@@ -0,0 +1,2 @@
+# JS projects
+A collection of projects to sharpen JS skills as a frontend developer
